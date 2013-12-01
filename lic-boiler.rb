f65abf0a8d5e6f2f803a0703518f85d7730657a7
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2013-12-01 17:39:50 -0600

    Add license to key files. Not that it’s particularly exciting stuff, really.

--- lic-boiler.rb
+++ lic-boiler.rb
@@ -11,21 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# == Schema Information
-#
-# Table name: subtopics
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  topic_id   :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
-class Subtopic < ActiveRecord::Base
-  belongs_to :topic
-  has_and_belongs_to_many :people
-  validates :name, presence: true, allow_blank: false, uniqueness: true
-  validates :topic, presence: true, allow_blank: false
-end
